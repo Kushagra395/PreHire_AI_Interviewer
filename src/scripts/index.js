c4d146c87e75a72efc2a7da79c7d808704e16b73
kushagra <kushagramalviya3@gmail.com>
2026-06-04 20:50:58 +0530

    changes in api version

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,7 +3,7 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
 const genAI = new GoogleGenerativeAI(import.meta.env?.VITE_GEMINI_API_KEY ) ;
-const model = genAI.getGenerativeModel({ model: "gemini-3-pro-preview" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 export const chatSession = model.startChat({
   generationConfig: {
     responseMimeType: 'application/json',
